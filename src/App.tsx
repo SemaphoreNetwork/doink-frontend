f778a1336272952c94972fd19627933f6bd86dd2
Jake Kidd <jcb.kdd@gmail.com>
2024-02-15 12:25:05 -0800

    filter input; add input state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import PasteIcon from "./paste-icon.svg";
 
 function App() {
+  const [address, setAddress] = useState<string>("");
+
+  const filterInput = function (value: string): string {
+    // Ensure first two chars are "0x".
+    if (value.length > 0) {
+      if (value[0] !== "0") {
+        return "";
+      }
+      if (value.length > 1) {
+        if (value[1] !== "x") {
+          return "0";
+        }
+
+        // Iterate through chars after "0x" to validate they are all hex.
+        const regEx = /[0-9a-fA-F]+/;
+        const body = value.slice(2);
+        let newValue = "0x";
+        for (const char of body) {
+          const isHex = regEx.exec(char);
+          if (isHex) {
+            newValue += char;
+          }
+        }
+        return newValue;
+      }
+    }
+
+    return value;
+  };
+
   return (
     <div className="container">
       <div className="form">
-        {/* <p>hello doinks</p> */}
         <label className="label">Address</label>
         <div className="inputBox">
           <input
@@ -14,9 +43,17 @@ function App() {
             placeholder="0xdoink"
             id="address-input"
             type="text"
+            value={address}
             style={{ flexGrow: 2 }}
+            onChange={(e) => {
+              setAddress(filterInput(e.target.value));
+            }}
           />
-          <button>
+          <button
+            onClick={async () => {
+              setAddress(await navigator.clipboard.readText());
+            }}
+          >
             <img src={PasteIcon} aria-hidden="true" />
           </button>
         </div>
